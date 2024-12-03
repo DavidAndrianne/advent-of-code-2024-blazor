19aa91d76b33277b506b88dc5b89db18ca471948
DavidAndrianne <r0426942@gmail.com>
2024-12-03 20:13:02 +0100

    Day 3 solution
    Refactor Day 2
    Clean-up unused files

--- AoC.2024.Blazor/Parsers/Week2/Day2Parser.cs
+++ AoC.2024.Blazor/Parsers/Week2/Day2Parser.cs
@@ -7,5 +7,5 @@ public static class Day2Parser
     public static IEnumerable<IEnumerable<int>> ParseIntLines(this string input)
         => input.Split("\n")
             .Select(x => x.Split(" ").Select(x => int.Parse(x)))
-            .ToList();
+            .ToArray();
 }
---
+++ AoC.2024.Blazor/Parsers/Week2/Day3Parser.cs
@@ -0,0 +1,44 @@
+﻿using AoC2024.Blazor.Extensions;
+using Microsoft.AspNetCore.Components.Forms;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+
+namespace AoC2024.Blazor.Parsers.Week2;
+
+public static class Day3Parser
+{
+    public static IEnumerable<string> ParseExpression(this string input, string expression)
+        => new Regex(expression)
+            .Matches(input)
+            .Select(x => x.ToString())
+            .ToArray();
+
+    public static IEnumerable<int> ParseInts(this string input)
+        => new Regex(@"\d+")
+            .Matches(input)
+                .Select(x => int.Parse(x.ToString()))
+                .ToArray();
+
+    public static IEnumerable<string> ParseDoMultitudes(this string input)
+    {
+        List<string> results = new();
+        bool isFirst = true;
+        foreach(var part in input.Split("don't()"))
+        {
+            if (isFirst)
+            {
+                isFirst = false;
+                results.AddRange(part.ParseExpression(@"mul\(\d+,\d+\)"));
+                continue;
+            }
+
+            var instructions = part.Split("do()");
+
+            var skipped = instructions[0];
+            var parsed = string.Join('|', instructions.Skip(1));
+            results.AddRange(parsed.ParseExpression(@"mul\(\d+,\d+\)"));
+        }
+
+        return results;
+    }
+}
